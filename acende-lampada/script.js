3c7b04eb9a06177150b9d292fc7dd316e1c0ed4d
priscila duarte <pgcosta@stefanini.com>
2022-07-13 18:32:06 -0300

    Adiciona funções

---
+++ acende-lampada/script.js
@@ -0,0 +1,19 @@
+const name = document.querySelector(".name")
+
+function turnOn() {
+    /* se clicar nesse botão, troque de imagem */
+    return
+}
+
+function turnOff()  {
+    /* se clicar nesse botão, troque de imagem */
+    return
+}
+
+function smashLamp() {
+    /* Se clicar duas vezes no botão, troca de imagem */
+}
+
+function changeColor() {
+    name.style.color = "blue";
+}
