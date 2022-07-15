574caa93a81d8fe3256474baf9f8bd5aae59bf08
priscila duarte <pgcosta@stefanini.com>
2022-07-15 14:43:03 -0300

    Finaliza parte 1

--- acende-lampada/script.js
+++ acende-lampada/script.js
@@ -2,20 +2,28 @@ const turnOn = document.getElementById('turnOn');
 const turnOff = document.getElementById('turnOff');
 const lamp = document.getElementById('lamp');
 
-
-function cliqueAqui() {
-    alert("clicou")
+function isLampBroken() {
+    return lamp.src.indexOf('quebrada') > -1
 }
 
 function lampOn() {
-    lamp.src = './img/ligada.jpg'
+    if (!isLampBroken()) {
+        lamp.src = './img/ligada.jpg'
+    }
 }
 
 function lampOff() {
-    lamp.src = './img/desligada.jpg'
+    if (!isLampBroken()) {
+        lamp.src = './img/desligada.jpg'
+    }
+}
+
+function lampBroken() {
+    lamp.src = './img/quebrada.jpg'
 }
 
 turnOn.addEventListener('click', lampOn);
 turnOff.addEventListener('click', lampOff);
 lamp.addEventListener('mouseover', lampOn)
 lamp.addEventListener('mouseleave', lampOff)
+lamp.addEventListener('dblclick', lampBroken)
