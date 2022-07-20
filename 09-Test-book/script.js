83d0f46e495f11af9a71e0aef2e3b1dd4d814529
priscila duarte <pgcosta@stefanini.com>
2022-07-20 15:04:24 -0300

    Inserindo elemento no html

--- 09-Test-book/script.js
+++ 09-Test-book/script.js
@@ -74,3 +74,35 @@ Carro.adicionarCor = function (novaCor) {
 }
 
 /* -------------FORMULÁRIOS-------------- */
+
+function validarForm() {
+    var val = document.getElementById("valido");
+    try {
+        var x = document.forms["meuForm"]["nome"].value;
+        if (x == null || x == "") {
+            throw "O nome deve ser preenchido";
+        }
+        var y = document.forms["meuForm"]["email"].value;
+        var atpos = y.indexOf("@");
+        var dotpos = y.lastIndexOf(".");
+
+        if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= y.length) {
+            throw "Digite um e-mail válido!";
+        }
+        return true;
+    } catch (err) {
+        val.style.color = "#FF0000";
+        val.innerHTML = "Erro: " + err;
+        return false;
+    }
+}
+
+/* ------------------------------ */
+function adicionar() {
+    var texto = document.getElementById("texto").value;
+    var para = document.createElement("p");
+    para.innerHTML = texto;
+
+    var corpo = document.getElementById("corpo");
+    corpo.appendChild(para);
+}
